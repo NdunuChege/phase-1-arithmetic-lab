d182058dc45b5b7d616d3b33fbdc0be03c5f1e14
NdunuChege <itsianndunu@gmail.com>
2024-12-02 18:35:42 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// 1. Create a variable called multiply that multiplies num1 and num2 to result in 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// 2. Create a variable called random that generates a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; 
+
+// 3. Create a variable called mod that calculates the remainder of dividing num3 by num4, with the remainder being 4
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; 
+
+// 4. Create a variable called max that finds the highest number in a set, with the value being 20
+let max = Math.max(2, 10.5, 12, 20); 
